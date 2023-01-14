11d3a5f601f021c744ed07134d15649c055aabab
kemmo47 <longkem123456@gmail.com>
2023-01-14 16:37:55 +0700

    commit 2

---
+++ routes/api/random-uuid.ts
@@ -0,0 +1,12 @@
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  GET(req) {
+    console.log(req);
+
+        const uuid = crypto.randomUUID();
+    return new Response(JSON.stringify(uuid), {
+      headers: { "Content-Type": "application/json" },
+    });
+  },
+};
